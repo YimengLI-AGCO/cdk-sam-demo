351d7d71d51ca07f58e65944b50d867454b6cc14
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-29 22:03:24 -0500

    removed unused comments in lambda stack

--- lib/cdk-sam-demo-stack.ts
+++ lib/cdk-sam-demo-stack.ts
@@ -2,61 +2,32 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as apigateway from '@aws-cdk/aws-apigateway';
-import { CfnParameter } from '@aws-cdk/core';
-import { Code } from '@aws-cdk/aws-lambda';
+import s3 = require('@aws-cdk/aws-s3');
 
 
 export class CdkSamDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.Table(this, 'SamTests', {
+    const table = new dynamodb.Table(this, 'SamTest', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      tableName: 'SamTest',
-      readCapacity: 6,
-      writeCapacity: 6
+      tableName: 'SamTest'
     });
 
-    const newTable = new dynamodb.Table(this, 'NewSamTest', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      tableName: 'NewSamTest-1'
+    const hello = new lambda.Function(this, 'MyFunction', {
+      functionName: 'hello-world',
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.lambda_handler',
+      code: lambda.Code.fromAsset('lambda_function')
     });
 
-    // const table2 = dynamodb.Table.fromTableArn(this, 'NewSamTest', table.tableArn)
-
-    // const bucketNameParam = new cdk.CfnParameter(this, "LambdaSourceBucketName");
-    // bucketNameParam.overrideLogicalId("LambdaSourceBucketName");
-
-    // const objectKeyParam = new cdk.CfnParameter(this, "LambdaSourceObjectKey");
-    // objectKeyParam.overrideLogicalId("LambdaSourceObjectKey");
-
-    // const hello = new lambda.Function(this, 'MyFunction', {
-    //   runtime: lambda.Runtime.PYTHON_3_7,
-    //   handler: 'index.lambda_handler',
-    //   code: lambda.Code.fromCfnParameters({
-    //     bucketNameParam,
-    //     objectKeyParam
-    //   })
-    // });
-
-    // const health = new lambda.Function(this, 'Table Exist', {
-    //   runtime: lambda.Runtime.PYTHON_3_7,
-    //   handler: 'table_exist.health_check',
-    //   code: lambda.Code.fromCfnParameters({
-    //     bucketNameParam: new CfnParameter(this, 'lambda2'),
-    //     objectKeyParam: new CfnParameter(this, 'key2')
-    //   }),
-    //   environment: {
-    //     'TABLE_NAME': 'NewSamTest',
-    //     'ENV': 'local'
-    //   }
-    // });
-    const createItem = new lambda.Function(this, 'Create Item', {
+    const createItem = new lambda.Function(this, 'CreateItem', {
       functionName: 'yimeng-fun-1',
       runtime: lambda.Runtime.PYTHON_3_7,
       handler: 'sam_test.create_handler',
       code: lambda.Code.asset('lambda_function')
     });
+    table.grantFullAccess(createItem);
 
     // const api = new apigateway.RestApi(this, 'sam-test-api', {
     //   restApiName: 'Sam Test'
@@ -67,6 +38,5 @@ export class CdkSamDemoStack extends cdk.Stack {
     // sam.addMethod('GET', samIntegration);
 
     // table.grantFullAccess(health);
-    table.grantWriteData(createItem);
   }
 }
