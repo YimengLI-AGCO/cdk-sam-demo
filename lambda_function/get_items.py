12272d957c6e081569cbbad8362603c9f2039b1d
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-30 17:00:01 -0500

    Lambda function to get items in status table

---
+++ lambda_function/get_items.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+import traceback
+
+TABLE_NAME = os.environ.get('TABLE_NAME')
+
+
+def handler(event, context):
+    """Get All items in ${TABLE_NAME}"""
+
+    dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb:8000/')
+    table = dynamodb.Table(TABLE_NAME)
+    try:
+        response = table.scan()
+        print(response)
+        return {'statusCode': 200, 'body': response.get('Items')}
+    except Exception as e:
+        traceback.print_exc()
+        return {'statusCode': 500, 'body': 'Failed to retrieve items'}
+
+
