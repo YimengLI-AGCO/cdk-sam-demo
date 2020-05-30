cf4a2c17762487b04142f5fb4de72b14da9bace5
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-30 16:33:26 -0500

    shell script to start the local API

---
+++ scripts/start_api.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\033[31mStart the Local API Gateway \033[0m"
+echo -e "[RUN]\033[35m sam local start-api --docker-network sam-demo \033[0m\n"
+sam local start-api --docker-network sam-demo
